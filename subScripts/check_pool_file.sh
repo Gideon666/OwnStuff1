1a81ca847f2e81e2854a247f9f36d26bfb74dc42
Jan Mitschke <jmitschke@gmx.de>
2018-07-17 11:48:13 +0200

    adds work_dir path in check_pool_file.sh to make script more flexible

--- subScripts/check_pool_file.sh
+++ subScripts/check_pool_file.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+work_dir=$2
+
 if [[ $1 != *"_"*  ]]; then
-    echo $1
+    echo "nothing to do "$1
     exit
 fi
+
+echo -e "creating Pool : "$1
 
 declare -A pool_files
 pool_ids=$(echo $1 | tr "_" "\n")
@@ -14,10 +18,10 @@ pool_ids=$(echo $1 | tr "_" "\n")
 #fi
 
 for id in $pool_ids; do
-    pool_files[${#pool_files[@]}]="${id}_pool.fasta"
+    pool_files[${#pool_files[@]}]="${work_dir}${id}_pool.fasta"
 done
 
 new_file_name=$1
 
-cat ${pool_files[@]} > "${new_file_name}_pool.fasta"
+cat ${pool_files[@]} > "${work_dir}${new_file_name}_pool.fasta"
     
